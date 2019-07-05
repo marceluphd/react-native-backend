6ea79a2807adab119a29b54ef38c1704a76fafda
Vineet Mishra <vineet.m@photoninfotech.net>
2019-07-05 14:18:27 +0530

    final commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -33,7 +33,9 @@ router.post("/login", (req, res) => {
         user.generateAuthToken().then((token) => {
             res.header({"x-auth": token}).send(user);
         });
-    });
+    }).catch((e) => {
+		    res.status(400).send(e);
+	  });
 });
 
 router.delete("/logout", authenticate, (req, res) => {
